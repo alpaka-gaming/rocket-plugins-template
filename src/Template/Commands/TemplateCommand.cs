9ec81468308fff386de27d73694f7e0b151f06ba
Enner Pérez <ennerperez@gmail.com>
2020-06-07 01:19:37 -0500

    Code optimization

--- src/Template/Commands/TemplateCommand.cs
+++ src/Template/Commands/TemplateCommand.cs
@@ -6,13 +6,13 @@ namespace Rocket.Plugins.Template.Commands
     public class TemplateCommand : IRocketCommand
     {
         public string Name => "template";
-        public string Syntax { get { return ""; } }
+        public string Syntax => "";
         public string Help => Plugin.Instance.Translate("template_help_message");
 
-        public List<string> Aliases { get { return new List<string>(); } }
-        public List<string> Permissions { get { return new List<string>(); } }
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string>();
 
-        public AllowedCaller AllowedCaller { get { return AllowedCaller.Both; } }
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
